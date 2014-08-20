875f7fae5ce2305c914a9371011793587ca65bdd
Jonathan Daugherty <jtd@galois.com>
2014-08-20 14:36:36 -0700

    weather: display filler on failure to get weather data

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -4,7 +4,12 @@ URL="http://api.wunderground.com/weatherstation/WXCurrentObXML.asp?ID="
 if [ ! -z "$WEATHER_STATION" ]
 then
     TEMP=$(curl -s "${URL}${WEATHER_STATION}" | grep temp_f | cut -d \> -f 2 | cut -d \< -f 1)
-    echo "${TEMP}°F"
+    if [ -z "${TEMP}" ]
+    then
+        echo "--°F"
+    else
+        echo "${TEMP}°F"
+    fi
 else
     echo "\$WEATHER_STATION not set"
 fi
