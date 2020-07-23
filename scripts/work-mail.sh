c1dce81402edcfc318c73e358f52c89224acc961
Jonathan Daugherty <cygnus@foobox.com>
2020-07-23 12:55:56 -0700

    Improve work mail script to check folders

--- scripts/work-mail.sh
+++ scripts/work-mail.sh
@@ -2,10 +2,28 @@
 
 set -e
 
-MAIL=~/mail/gmail-galois/INBOX/new
-N=$(ls $MAIL | wc -l | awk '{ print $1 }')
+ROOT=~/mail/gmail-galois
+INBOX=$ROOT/INBOX/new
+INBOX_N=$(ls $INBOX | wc -l | awk '{ print $1 }')
+OTHER_N=$(find $(find $ROOT -type d -name new) -type f | grep -v INBOX | wc -l | awk '{ print $1 }')
 
-if [ $N -gt 0 ]
+s=""
+
+if [ $INBOX_N -gt 0 ]
 then
-    echo "g:$N"
+    s="$s$INBOX_N"
+fi
+
+if [ $OTHER_N -gt 0 ]
+then
+    if [ $INBOX_N -eq 0 ]
+    then
+        s="0"
+    fi
+    s="$s,$OTHER_N"
+fi
+
+if [ ! -z "$s" ]
+then
+    echo "g:$s"
 fi
